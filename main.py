f60e857ebb3e9e6430eb448c3547cad9fe96fe7b
EJBEAN2op <danonymousarmy@gmail.com>
2021-10-18 17:54:24 +0530

    run animation

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import pygame
 from PIL import Image
 import math
+import os
 
 
-
+# pillow to crop sprites dynamically omegalul
 # x = 0
 # value = math.floor(240 / 4) - 12
 # val = value
 # for i in range(5):
-#     img = Image.open('Assets\images\TeamGunner_By_SecretHideout_060519\CHARACTER_SPRITES\Green\Gunner_Green_Idle.png')
+#     img = Image.open('images\TeamGunner_By_SecretHideout_060519\CHARACTER_SPRITES\Green\Gunner_Green_Run.png')
 #     # img.show()
 #     # cropped = img.crop((x, 0, val, 45))
 #     cropped = img.crop((x, 0, val, 45))
 #     # cropped.show()
-#     cropped.save(f'Assets/player/Idle/{i}.png')
+#     cropped.save(f'Assets/player/Run/{i}.png')
 #     x += value
 #     val += value
 
@@ -52,26 +53,38 @@ class Player(pygame.sprite.Sprite):
         self.direction = 1  # facing right
         self.flip = False
         self.speed = speed
+        self.alive = True
 
         #sprite animations
-        self.update_time = pygame.time.get_ticks();
+        self.update_time = pygame.time.get_ticks()
         self.animation_list = []
         self.frame_index = 0
         
         #sprite action [idle, run] etc
         self.action = 0 #initially idle
-        animation_types = ['Idle']
+        animation_types = ['Idle', 'Run']
         
-        for i in range(5):
-            img = pygame.image.load(f'Assets/{self.Ctype}/Idle/{i}.png')
-            img = pygame.transform.scale(
+        for animation in animation_types:
+            
+            temp_list = []
+            
+            frame_count = len(os.listdir(f'Assets/{self.Ctype}/{animation}'))
+            
+            for i in range(frame_count):
+                img = pygame.image.load(f'Assets/{self.Ctype}/{animation}/{i}.png')
+                img = pygame.transform.scale(
                 img, (int(img.get_width() * scale),
                       int(img.get_height() * scale))
-            )
-            self.animation_list.append(img)
+                )   
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
+            
+        print(self.animation_list)
+                
+
 
         # render sprites
-        self.image = self.animation_list[self.frame_index]
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -99,13 +112,22 @@ class Player(pygame.sprite.Sprite):
     def update_animation(self):
         ANIMATION_COOLDOWN = 100
         #update based on index
-        self.image = self.animation_list[self.frame_index]
+        self.image = self.animation_list[self.action][self.frame_index]
+        # print(self.image)
         #check if enough time has passed
         if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
             self.frame_index += 1
             self.update_time = pygame.time.get_ticks()
-        if self.frame_index >= len(self.animation_list):
+        if self.frame_index >= len(self.animation_list[self.action]):
             self.frame_index = 0
+            
+    def update_action(self, new_action):
+        #check if the new action is different to the previous one
+        if new_action != self.action:
+            self.action = new_action
+            #update the animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
         
     def draw(self):
         screen.blit(
@@ -129,10 +151,21 @@ while run:
     draw_background()
 
     player.update_animation()
-    player.update()
+    # player.update()
     player.draw()
+    
+    if player.alive:
+        # if player.in_air:
+        #     player.update_action(2)#2: jump
+        # elif moving_left or moving_right:
+        #     player.update_action(1)#1: run
+        # else:
+        if moving_left or moving_right:
+            player.update_action(1)
+        else:
+            player.update_action(0)#0: idle
+        player.move(moving_left, moving_right)
     # enemy.draw()
-    player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
